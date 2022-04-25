32bf2129733c43517ed4caccd3c86e181ec174fe
mhensel1 <98911870+mhensel1@users.noreply.github.com>
2022-04-24 20:22:27 -0400

    Minor update to game controller
    Adds db function to update piece info in game controller

--- GroupChess/src/edu/ycp/cs320/chess/controller/GameController.java
+++ GroupChess/src/edu/ycp/cs320/chess/controller/GameController.java
@@ -2,8 +2,13 @@ package edu.ycp.cs320.chess.controller;
 
 import edu.ycp.cs320.chess.model.ChessGame;
 import edu.ycp.cs320.chess.model.ChessPiece;
+import edu.ycp.cs320.booksdb.persist.DatabaseProvider;
+import edu.ycp.cs320.booksdb.persist.IDatabase;
 import edu.ycp.cs320.chess.model.BoardSpace;
 import edu.ycp.cs320.chess.model.ChessBoard;
+import edu.ycp.cs320.booksdb.model.Pair;
+import java.util.List;
+import edu.ycp.cs320.booksdb.model.Piece;
 
 public class GameController {
 	private ChessGame Gamemodel;
@@ -31,6 +36,9 @@ public class GameController {
 	public boolean canMove(ChessBoard board, BoardSpace start, BoardSpace end) {
 		this.Boardmodel = board;
 		// check for pieces at the starting and ending spot and see if a move can be made
+		IDatabase db = DatabaseProvider.getInstance();
+		//List<Piece> piece = db.findPieceByXY(xCor, yCor);
+		
 		return Boardmodel.getSpace(start.getX(), start.getY()).getPiece().canMove(board,Boardmodel.getSpace(start.getX(), start.getY()), Boardmodel.getSpace(end.getX(), end.getY()));
 	}
 	
@@ -50,6 +58,10 @@ public class GameController {
 			end.getPiece().setHasMoved(true);
 			
 			System.out.println("Move Succesfull");
+			IDatabase db = DatabaseProvider.getInstance();
+			Pair<Integer, Integer> pair = db.updatePieceInfoByCoords(start.getX(), start.getY(), end.getX(), end.getY(), false);
+			System.out.println("Piece info updated. new Coordinates: " + pair.getLeft() + ", " + pair.getRight());
+			
 			
 			System.out.println("New Start Space/Piece : " + Boardmodel.getSpace(start.getX(), start.getY()).getPiece());
 			System.out.println("New End Space/Piece: " + Boardmodel.getSpace(end.getX(), end.getY()).getPiece());
